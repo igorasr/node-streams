3cb3776042b48a2550a289b61f62e66583455d0e
igorasr <igoraugustosouza00@gmail.com>
2023-06-11 11:42:27 -0300

    fix: Corrigindo dados quebrados.

--- index.js
+++ index.js
@@ -1,78 +1,61 @@
-import fs from 'node:fs';
-import { pipeline } from 'node:stream/promises';
-import ThrottleRequest from './ThrottleRequest.js';
+import { pipeline } from "node:stream/promises";
+import { Transform } from "node:stream";
+import fs from "node:fs";
+import readline from "node:readline";
+import ThrottleRequest from "./ThrottleRequest.js";
 
-const readable = fs.createReadStream('./resources/mock-data.csv', { encoding: 'utf8'})
-const writable = fs.createWriteStream('./resources/result.json');
+const readable = fs.createReadStream("./resources/mock-data.csv", {
+  encoding: "utf8",
+});
+const writable = fs.createWriteStream("./resources/result.json");
+const readLine = readline.createInterface({
+  input: readable,
+});
 
-/**
- * It receives the data, processes it by indexing the headers to the current data 
- * and returns a string of a formatted object.
- * @param {Object} data 
- * @returns {String} 
- */
-function csvToJson({ line, headers }) {
+const FLOW_RATE = 1000;
 
-    let formatedObject = {};
+function handleLine() {
+  let firstChunk = true;
+  let headers = [];
+  let formatedObject = {};
 
-    headers.forEach((key, index) => formatedObject[key.replace(/\W/g, '').toLowerCase()] = line[index]);
+  return new Transform({
+    transform(chunk, encoding, callback) {
+      if (firstChunk) {
+        // Get Headers
+        headers = chunk
+          .toString()
+          .split(",")
+          .map((key) =>
+            key.trim().replace(/\s/g, "_").replace(/\W/g, "").toLowerCase()
+          );
+        firstChunk = false;
+        return callback();
+      }
 
-    return JSON.stringify(formatedObject) + ',\n';
-}
+      const splitedLine = chunk.toString().split(",");
 
-/**
- * Receives a function that will be executed to process the CSV lines
- * @param {Function} callback 
- * @returns {Function}
- */
-function transform(callback) {
+      headers.forEach(
+        (key, index) => (formatedObject[key] = splitedLine[index])
+      );
 
-    return async function* (source) { // Receives and forwards already formatted chunks to the stream
-        source.setEncoding('utf8')
-        let firstChunk = true;
-        let headers = [];
-
-        for await (const chunk of source) {
-            for (const line of chunk.split('\r\n')) {
-                if (firstChunk) { // getting headers on the first row for indexing the datakia
-                    headers = line.split(',');
-                    firstChunk = false;
-                    continue;
-                }
-                yield callback({ line: line.split(','), headers })
-            }
-
-        }
-    }
-
+      this.push(JSON.stringify(formatedObject) + ",\r\n");
+      callback();
+    },
+  });
 }
 
 const throttle = new ThrottleRequest({
-    objectMode: true, flowRatePerSecond: 10000
+  objectMode: true,
+  flowRatePerSecond: FLOW_RATE,
 });
 
-/**
- * Main function for pipeline initialization
- * @returns {Void}
- */
-async function run() {
+console.time("Pipeline");
 
-    try {
-        await pipeline(
-            readable,
-            transform(csvToJson),
-            writable
-        );
-    } catch (error) {
-        console.error(error.message);
-        return false;
-    }
+// With throttling
+// await pipeline(readLine, handleLine(), throttle, writable);
 
-    return true;
-}
+// Without Throttling
+await pipeline(readLine, handleLine(), writable);
 
-console.time('Pipeline');
-
-await run();
-
-console.timeEnd('Pipeline');
+console.timeEnd("Pipeline");
